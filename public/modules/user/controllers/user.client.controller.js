833b8db5f1ec7449988bfe829ca4ec60081ccdb3
davaaana <davaana_bit@yahoo.com>
2015-10-08 10:37:26 -0800

    user change

--- public/modules/user/controllers/user.client.controller.js
+++ public/modules/user/controllers/user.client.controller.js
@@ -1,22 +1,14 @@
 'use strict';
 
-angular.module('user').controller('LoginController', ['$rootScope', '$scope','$http', 'UserSrv','Authentication','$location',
+angular.module('user').controller('LoginController', ['$rootScope', '$scope','$http', 'UserAuthSrv','Authentication','$location',
     function ($rootScope, $scope,$http, UserSrv,Authentication,$location) {
         $scope.title = 'asdsadsad';
         $scope.username = 'davaana';
         $scope.password = '123';
+
         $scope.signin = function () {
-            $http({
-                method: "post",
-                url: '/signin',
-                headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-                data: $.param({username: $scope.username, password: $scope.password})
-            }).success(function (response) {
-                Authentication.user = response;
-                window.location.href = '#!/admin';
-            }).error(function (response) {
-                $scope.error = response.message;
-            });
+            var params = {username:$scope.username,password:$scope.password};
+            UserSrv.signin(params);
         };
     }
 ]);
