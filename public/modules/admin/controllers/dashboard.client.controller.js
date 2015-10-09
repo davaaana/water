5a3f5baaabe663dd8af8e79bbe25a1f68dd3cec0
Amarsaikhan <amaraa_it@yahoo.com>
2015-10-09 09:14:15 +0800

    login css хийв

--- public/modules/admin/controllers/dashboard.client.controller.js
+++ public/modules/admin/controllers/dashboard.client.controller.js
@@ -3,7 +3,7 @@
 angular.module('admin').controller('DashboardController', ['$rootScope', '$scope','$http', 'UserSrv','Authentication','$location',
     function ($rootScope, $scope,$http, UserSrv,Auth,$location) {
         if(!Auth.user){ return $location.path('/login');}
-        $scope.title = '?????????? ?????';
+        $scope.title = 'Хоосо   н';
         $http.get('/roles').success(function (response) {
             $scope.roles = response;
         });
